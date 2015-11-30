b644998bc26fc40c5cd5bf50acdc7715758799d1
thibthibaut <thibo.v@gmail.com>
2015-11-30 15:22:04 +0100

    code cleanup

--- Projet_Techno/Form1.cs
+++ Projet_Techno/Form1.cs
@@ -14,8 +14,6 @@ namespace Projet_Techno
     {
         delegate void SetTextCallback(string text);
 
-        string name;
-
         public Form1()
         {
    
@@ -34,62 +32,8 @@ namespace Projet_Techno
             // Add Data receiver to the serial port
             serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
 
-            // Set default name
-            name = textBox2.Text;
-        }
+             }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (textBox1.Text != "")
-                {
-                    // Create string to send
-                    //string text = "[" + DateTime.Now.ToString("HH:mm:ss") + "] " + name + " : " + textBox1.Text + "\n";
-                    string text = textBox1.Text;
-                    int toSend = int.Parse(text);
-                    // Write your message
-                    richTextBox1.AppendText(text);
-                    byte[] b = BitConverter.GetBytes(toSend);
-                    serialPort1.Write(b, 0, 1);
-
-                    // Send to Serial Port
-                   // serialPort1.Write(int.Parse(text) + "" );
-        
-         
-                    // Erase text box
-                    textBox1.Text = "";
-                }
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Please select a port", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            if (textBox1.Text.Length > 1)
-            {
-                char t = textBox1.Text.ElementAt(textBox1.Text.Length - 1);
-                if (t == '\n')
-                {
-                    button1_Click(this, e);
-                    textBox1.Text = "";
-                }
-            }
-        }
-
-
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -98,8 +42,8 @@ namespace Projet_Techno
             {   
                 if (serialPort1.IsOpen)
                     serialPort1.Close();
-             
-                serialPort1.PortName = Convert.ToString(comboBox1.Text);
+
+                serialPort1.PortName = "COM5";// Convert.ToString(comboBox1.Text);
                 Console.Write(serialPort1.PortName);
                 serialPort1.Open();
             }
@@ -122,34 +66,15 @@ namespace Projet_Techno
             // Get Data from the sender
             string indata = sp.ReadLine();
 
-            // Byte[] b = BitConverter.GetBytes(indata);
-
-            //Console.Write(indata); // Debug   
-           // Random r = new Random();
-               
-           // if (r.Next(1, 10) == 1)     
-                this.BeginInvoke(new SetTextCallback(SetText),indata);
+           this.BeginInvoke(new SetTextCallback(SetText),indata);
         }
 
         private void SetText(string text)
         {
-            this.richTextBox1.Text = text ;
+            //this.richTextBox1.Text = text ;
             Player1.Location = new Point(8, (int.Parse(text) - 8)*8);
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            name += textBox2.Text;
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
